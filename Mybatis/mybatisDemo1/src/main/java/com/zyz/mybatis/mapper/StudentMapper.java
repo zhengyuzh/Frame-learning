d94204ac25d971b315fff939887adcc7a04993be
18348375641 <a11083879A>
2023-11-21 21:50:24 +0800

    2、测试CURD 增、删、改、查、模糊查询    修改 通过两种方式 一个是对象，另一个是map。

--- Mybatis/mybatisDemo1/src/main/java/com/zyz/mybatis/mapper/StudentMapper.java
+++ Mybatis/mybatisDemo1/src/main/java/com/zyz/mybatis/mapper/StudentMapper.java
@@ -3,6 +3,7 @@ package com.zyz.mybatis.mapper;
 
 import com.zyz.mybatis.entity.Student;
 
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -21,10 +22,56 @@ public interface StudentMapper {
     List<Student> queryAll();
 
     /**
+     * @description: 模糊查询
+     * @author: zhengyuzhu
+     * @date: 2023/11/21 21:39
+     * @param: like
+     * @return: java.util.List<com.zyz.mybatis.entity.Student>
+     **/
+    List<Student> getStudentLike(String value);
+
+    /**
      * @description: 新增学生
      * @author: zhengyuzhu
      * @date: 2023/11/16 22:34
      * @param: student
+     *
+     * @return*/
+    Integer addStudent(Student student);
+
+    /**
+     * @description: 根据学号查询学生
+     * @author: zhengyuzhu
+     * @date: 2023/11/20 22:43
+     * @param: stuNo
+     * @return: com.zyz.mybatis.entity.Student
      **/
-    void addStudent(Student student);
+    Student queryStudentById(Integer stuNo);
+
+    /**
+     * @description: 修改学生信息
+     * @author: zhengyuzhu
+     * @date: 2023/11/20 22:45
+     * @param: student
+     * @return: java.lang.Integer
+     **/
+    Integer updateStudent(Student student);
+
+    /**
+     * @description: 通过Map 修改学生信息
+     * @author: zhengyuzhu
+     * @date: 2023/11/21 21:08
+     * @param: studentMap
+     * @return: java.lang.Integer
+     **/
+    Integer updateStudentMap(HashMap<String,Object> studentMap);
+
+    /**
+     * @description:删除学生 通过 学号
+     * @author: zhengyuzhu
+     * @date: 2023/11/20 22:45
+     * @param: stuNo
+     * @return: java.lang.Integer
+     **/
+    Integer deleteStudentById(Integer stuNo);
 }
--- Mybatis/mybatisDemo1/src/test/java/TestStudent.java
+++ Mybatis/mybatisDemo1/src/test/java/TestStudent.java
@@ -4,6 +4,7 @@ import com.zyz.mybatis.utils.MybatisUtils;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -44,6 +45,175 @@ public class TestStudent {
          * Student{stuNo=1, stuName='张三', cardID=1115, classID=1}
          * Student{stuNo=2, stuName='李四', cardID=1116, classID=2}
          * Student{stuNo=3, stuName='王五', cardID=1117, classID=3}
+         *
+         **/
+
+    }
+
+    /**
+     * @description: 修改对象信息 通过Map
+     * @author: zhengyuzhu
+     * @date: 2023/11/20 23:02
+     **/
+    @Test
+    public void test02(){
+        //第一步：获得SqlSession对象
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+
+        //方式一：getMapper
+        StudentMapper studentMapper = sqlSession.getMapper(StudentMapper.class);
+        HashMap<String, Object> stuMap = new HashMap<>();
+        stuMap.put("stuNo",1);
+        stuMap.put("stuName","李白");
+        stuMap.put("cardid",6);
+        stuMap.put("classid",6);
+        Integer rs = studentMapper.updateStudentMap(stuMap);
+        Student student = studentMapper.queryStudentById(1);
+        System.out.println(student);
+
+
+        //关闭SqlSession
+        sqlSession.close();
+
+        /**
+         *      输出如下 :
+         *
+         * Student{stuNo=1, stuName='张三', cardID=1115, classID=1}
+         * Student{stuNo=2, stuName='李四', cardID=1116, classID=2}
+         * Student{stuNo=3, stuName='王五', cardID=1117, classID=3}
+         *
+         **/
+
+    }
+
+    /**
+     * @description: 修改对象信息 通过 Student 对象
+     * @author: zhengyuzhu
+     * @date: 2023/11/20 23:02
+     **/
+    @Test
+    public void test03(){
+        //第一步：获得SqlSession对象
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+
+        //方式一：getMapper
+        StudentMapper studentMapper = sqlSession.getMapper(StudentMapper.class);
+
+        Student studentBefore = studentMapper.queryStudentById(0);
+        System.out.println("修改前的学生信息 : " + studentBefore);
+
+        Student student = new Student();
+        student.setStuNo(0);
+        student.setStuName("麻子嘞");
+        student.setClassID(6);
+        student.setCardID(6);
+        studentMapper.updateStudent(student);
+        sqlSession.commit();//将数据保存到数据库
+
+        Student studentAfter = studentMapper.queryStudentById(0);
+        System.out.println("修改后的学生信息 : " + studentAfter);
+
+        //关闭SqlSession
+        sqlSession.close();
+
+        /**
+         *      输出如下 :
+         *
+         *  修改前的学生信息 : Student{stuNo=0, stuName='李四', cardID=8, classID=8}
+         *  修改后的学生信息 : Student{stuNo=0, stuName='麻子嘞', cardID=6, classID=6}
+         *
+         **/
+
+    }
+
+    /**
+     * @description: 添加对象
+     * @author: zhengyuzhu
+     * @date: 2023/11/21 21:06
+     **/
+    @Test
+    public void test04(){
+        //第一步：获得SqlSession对象
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+
+        //方式一：getMapper
+        StudentMapper studentMapper = sqlSession.getMapper(StudentMapper.class);
+        Student student = new Student();
+        student.setStuNo(9);
+        student.setStuName("武松");
+        student.setClassID(8);
+        student.setCardID(8);
+        studentMapper.addStudent(student);
+        sqlSession.commit();//将数据保存到数据库
+        Student studentRs = studentMapper.queryStudentById(9);
+        System.out.println(studentRs);
+
+
+        //关闭SqlSession
+        sqlSession.close();
+
+        /**
+         *      输出如下 :
+         *      Student{stuNo=9, stuName='武松', cardID=8, classID=8}
+         *
+         **/
+
+    }
+
+    /**
+     * @description: 删除对象
+     * @author: zhengyuzhu
+     * @date: 2023/11/21 21:07
+     **/
+    @Test
+    public void test05(){
+        //第一步：获得SqlSession对象
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+
+        StudentMapper studentMapper = sqlSession.getMapper(StudentMapper.class);
+        studentMapper.deleteStudentById(9);
+        sqlSession.commit();//将数据保存到数据库
+        Student studentRs = studentMapper.queryStudentById(9);
+        System.out.println(studentRs);
+
+        //关闭SqlSession
+        sqlSession.close();
+
+        /**
+         *      输出如下 :
+         *      null
+         *
+         **/
+
+    }
+
+
+    /**
+     * @description: 模糊查询
+     * @author: zhengyuzhu
+     * @date: 2023/11/21 21:41
+     **/
+    @Test
+    public void test06(){
+        //第一步：获得SqlSession对象
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+
+        //方式一：getMapper
+        StudentMapper studentMapper = sqlSession.getMapper(StudentMapper.class);
+        List<Student> userList = studentMapper.getStudentLike("%李%");
+
+        for (Student student : userList) {
+            System.out.println(student);
+        }
+
+        //关闭SqlSession
+        sqlSession.close();
+
+        /**
+         *      输出如下 :
+         *
+         * Student{stuNo=2, stuName='李四', cardID=1116, classID=2}
+         * Student{stuNo=8, stuName='李老头', cardID=8, classID=8}
          *
          **/
 

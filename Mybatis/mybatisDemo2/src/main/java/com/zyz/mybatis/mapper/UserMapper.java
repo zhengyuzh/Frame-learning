ae10505d63d58afc85d78e1a9bb1100fb20e51ca
18348375641 <a11083879A>
2023-11-22 16:14:14 +0800

    10、使用注解开发 增、删、改、查。实体类 和 数据库字段不一致的情况。进行结果集的映射

--- Mybatis/mybatisDemo2/src/main/java/com/zyz/mybatis/mapper/UserMapper.java
+++ Mybatis/mybatisDemo2/src/main/java/com/zyz/mybatis/mapper/UserMapper.java
@@ -1,7 +1,7 @@
 package com.zyz.mybatis.mapper;
 
 import com.zyz.mybatis.entity.User;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 
@@ -12,6 +12,18 @@ import java.util.List;
  * @Description: User 实体类
  */
 public interface UserMapper {
+
+    /**
+     *
+     * 关于@Param()注解
+     *
+     * 基本类型的参数或者String类型，需要加上
+     * 引用类型不需要加
+     * 如果只有一个基本类型的话，可以忽略，但是建议都加上！
+     * 我们在SQL中引用的就是我们这里的@Param("")中设定的属性名！
+     *
+     **/
+
     /**
      * @description: 查询用户
      * @author: zhengyuzhu
@@ -19,6 +31,56 @@ public interface UserMapper {
      * @return: java.util.List<com.zyz.mybatis.entity.User>
      **/
     @Select("select * from user")
+    @Results(id = "UserMap",value = {
+            @Result(id=true,column = "id",property = "id"),
+            @Result(column = "name",property = "name"),
+            @Result(column = "pwd",property = "password")
+    })
     List<User> getUsers();
+
+    /**
+     * @description: 查询用户 通过 用户ID 方法存在多个参数，所有参数前面必须加上@Param("id")注解
+     * @author: zhengyuzhu
+     * @date: 2023/11/22 15:12
+     * @param: id
+     * @return: com.zyz.mybatis.entity.User
+     **/
+    @Select("select * from user where id=#{id}")
+    @ResultMap(value = ("UserMap"))
+    User getUserById(@Param("id") int id);
+
+
+    /**
+     * @description: 添加用户
+     * @author: zhengyuzhu
+     * @date: 2023/11/22 15:11
+     * @param: user
+     * @return: int
+     **/
+    @Insert("insert into user (id,name,pwd) values(#{id},#{name},#{password})")
+    int addUser(User user);
+
+    /**
+     * @description: 修改用户
+     * @author: zhengyuzhu
+     * @date: 2023/11/22 15:11
+     * @param: user
+     * @return: int
+     **/
+    @Update("update user set name=#{name},pwd=#{password} where id=#{id}")
+    int updateUser(User user);
+
+
+    /**
+     * @description:删除用户  可以通过@Param("") 给传递的参数 修改别名
+     * @author: zhengyuzhu
+     * @date: 2023/11/22 15:11
+     * @param: id
+     * @return: int
+     **/
+    @Delete("delete from user where id = #{uid}")
+    int deleteUser(@Param("uid") int id);
+
+
 
 }
--- Mybatis/mybatisDemo2/src/test/java/TestUser.java
+++ Mybatis/mybatisDemo2/src/test/java/TestUser.java
@@ -48,6 +48,166 @@ public class TestUser {
          * User{id=6, name='小明', password='null'}
          *
          *
+         * 采用结果集映射后
+         *
+         * [com.zyz.mybatis.mapper.UserMapper.getUsers]-==>  Preparing: select * from user
+         * [com.zyz.mybatis.mapper.UserMapper.getUsers]-==> Parameters:
+         * [com.zyz.mybatis.mapper.UserMapper.getUsers]-<==      Total: 7
+         * User{id=1, name='张三', password='123'}
+         * User{id=2, name='李四', password='456'}
+         * User{id=3, name='王五', password='789'}
+         * User{id=4, name='麻子', password='321'}
+         * User{id=5, name='李白', password='654'}
+         * User{id=6, name='小明', password='987'}
+         * User{id=7, name='小红', password='666'}
+         *
+         *
+         **/
+    }
+
+    /**
+     * @description: 查询 通过ID
+     * @author: zhengyuzhu
+     * @date: 2023/11/22 15:15
+     **/
+    @Test
+    public void testDemo2(){
+        //第一步：获得SqlSession对象
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        User user = mapper.getUserById(1);//查询
+
+        System.out.println(user);
+        sqlSession.close();
+
+        /**
+         * 输出如下：
+         *
+         * [com.zyz.mybatis.mapper.UserMapper.getUserById]-==>  Preparing: select * from user where id=?
+         * [com.zyz.mybatis.mapper.UserMapper.getUserById]-==> Parameters: 1(Integer)
+         * [com.zyz.mybatis.mapper.UserMapper.getUserById]-<==      Total: 1
+         * User{id=1, name='张三', password='null'}
+         *
+         * 复用结果集映射
+         * [com.zyz.mybatis.mapper.UserMapper.getUserById]-==>  Preparing: select * from user where id=?
+         * [com.zyz.mybatis.mapper.UserMapper.getUserById]-==> Parameters: 1(Integer)
+         * [com.zyz.mybatis.mapper.UserMapper.getUserById]-<==      Total: 1
+         * User{id=1, name='张三', password='123'}
+         *
+         **/
+    }
+
+    /**
+     * @description: 添加用户
+     * @author: zhengyuzhu
+     * @date: 2023/11/22 15:15
+     **/
+    @Test
+    public void testDemo3(){
+        //第一步：获得SqlSession对象
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        User user = new User();
+        user.setId(8);
+        user.setName("小青");
+        user.setPassword("888");
+        mapper.addUser(user);//添加
+        User userRs = mapper.getUserById(8);
+        System.out.println("新添加用户 : " + userRs);
+        sqlSession.close();
+
+        /**
+         * 输出如下：
+         *
+         * [com.zyz.mybatis.mapper.UserMapper.addUser]-==>  Preparing: insert into user (id,name,pwd) values(?,?,?)
+         * [com.zyz.mybatis.mapper.UserMapper.addUser]-==> Parameters: 8(Integer), 小青(String), 888(String)
+         * [com.zyz.mybatis.mapper.UserMapper.addUser]-<==    Updates: 1
+         * [com.zyz.mybatis.mapper.UserMapper.getUserById]-==>  Preparing: select * from user where id=?
+         * [com.zyz.mybatis.mapper.UserMapper.getUserById]-==> Parameters: 8(Integer)
+         * [com.zyz.mybatis.mapper.UserMapper.getUserById]-<==      Total: 1
+         * 新添加用户 : User{id=8, name='小青', password='888'}
+         *
+         **/
+    }
+
+    /**
+     * @description: 修改用户
+     * @author: zhengyuzhu
+     * @date: 2023/11/22 15:15
+     **/
+    @Test
+    public void testDemo4(){
+        //第一步：获得SqlSession对象
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        User userRs = mapper.getUserById(8);
+        System.out.println("未修改前用户信息 : " + userRs);
+
+        User user = new User();
+        user.setId(8);
+        user.setName("小紫");
+        user.setPassword("999");
+        mapper.updateUser(user);//修改
+
+        User userRss = mapper.getUserById(8);
+        System.out.println("修改后用户信息 : " + userRss);
+
+        sqlSession.close();
+
+        /**
+         * 输出如下：
+         *
+         * [com.zyz.mybatis.mapper.UserMapper.getUserById]-==>  Preparing: select * from user where id=?
+         * [com.zyz.mybatis.mapper.UserMapper.getUserById]-==> Parameters: 8(Integer)
+         * [com.zyz.mybatis.mapper.UserMapper.getUserById]-<==      Total: 1
+         * 未修改前用户信息 : User{id=8, name='小青', password='888'}
+         * [com.zyz.mybatis.mapper.UserMapper.updateUser]-==>  Preparing: update user set name=?,pwd=? where id=?
+         * [com.zyz.mybatis.mapper.UserMapper.updateUser]-==> Parameters: 小紫(String), 999(String), 8(Integer)
+         * [com.zyz.mybatis.mapper.UserMapper.updateUser]-<==    Updates: 1
+         * [com.zyz.mybatis.mapper.UserMapper.getUserById]-==>  Preparing: select * from user where id=?
+         * [com.zyz.mybatis.mapper.UserMapper.getUserById]-==> Parameters: 8(Integer)
+         * [com.zyz.mybatis.mapper.UserMapper.getUserById]-<==      Total: 1
+         * 修改后用户信息 : User{id=8, name='小紫', password='999'}
+         *
+         **/
+    }
+
+    /**
+     * @description: 删除用户
+     * @author: zhengyuzhu
+     * @date: 2023/11/22 15:15
+     **/
+    @Test
+    public void testDemo5(){
+        //第一步：获得SqlSession对象
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+
+        User userRs = mapper.getUserById(8);
+        System.out.println("删除前用户信息 : " + userRs);
+
+        mapper.deleteUser(8);//删除
+
+        User userRss = mapper.getUserById(8);
+        System.out.println("删除后用户信息 : " + userRss);
+
+        sqlSession.close();
+
+        /**
+         * 输出如下：
+         *
+         * [com.zyz.mybatis.mapper.UserMapper.getUserById]-==>  Preparing: select * from user where id=?
+         * [com.zyz.mybatis.mapper.UserMapper.getUserById]-==> Parameters: 8(Integer)
+         * [com.zyz.mybatis.mapper.UserMapper.getUserById]-<==      Total: 1
+         * 删除前用户信息 : User{id=8, name='小紫', password='999'}
+         * [com.zyz.mybatis.mapper.UserMapper.deleteUser]-==>  Preparing: delete from user where id = ?
+         * [com.zyz.mybatis.mapper.UserMapper.deleteUser]-==> Parameters: 8(Integer)
+         * [com.zyz.mybatis.mapper.UserMapper.deleteUser]-<==    Updates: 1
+         * [com.zyz.mybatis.mapper.UserMapper.getUserById]-==>  Preparing: select * from user where id=?
+         * [com.zyz.mybatis.mapper.UserMapper.getUserById]-==> Parameters: 8(Integer)
+         * [com.zyz.mybatis.mapper.UserMapper.getUserById]-<==      Total: 0
+         * 删除后用户信息 : null
+         *
          **/
     }
 }

a805d9ae44aebf9ac6e78a0cfb52f48e5939965b
18348375641 <a11083879A>
2023-11-26 15:33:12 +0800

    9、文件上传和下载

---
+++ SpringMVC代码/springMVC-demo3/src/main/java/com/zyz/mvc/controller/FileController.java
@@ -0,0 +1,87 @@
+package com.zyz.mvc.controller;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpSession;
+import java.io.*;
+import java.util.UUID;
+
+/**
+ * @author zyz
+ * @version 1.0
+ * @data 2022/12/12 19:57
+ * @Description:
+ */
+@Controller
+public class FileController {
+
+
+    /**
+     * 测试下载
+     * @param session
+     * @return
+     * @throws IOException
+     */
+    @RequestMapping("/testDown")
+    public ResponseEntity<byte[]> testResponseEntity(HttpSession session) throws IOException {
+        //获取ServletContext对象
+        ServletContext servletContext = session.getServletContext();
+        //获取服务器中文件的真实路径
+        String realPath = servletContext.getRealPath("/static/img/1.jpg");
+        //创建输入流
+        InputStream is = new FileInputStream(realPath);
+        //创建字节数组
+        byte[] bytes = new byte[is.available()];
+        //将流读到字节数组中
+        is.read(bytes);
+        //创建HttpHeaders对象设置响应头信息
+        MultiValueMap<String, String> headers = new HttpHeaders();
+        //设置要下载方式以及下载文件的名字
+        headers.add("Content-Disposition", "attachment;filename=1.jpg");
+        //设置响应状态码
+        HttpStatus statusCode = HttpStatus.OK;
+        //创建ResponseEntity对象
+        ResponseEntity<byte[]> responseEntity = new ResponseEntity<>(bytes, headers, statusCode);
+        //关闭输入流
+        is.close();
+        return responseEntity;
+    }
+
+
+    /**
+     * 测试上传
+     * @param photo
+     * @param session
+     * @return
+     * @throws IOException
+     */
+    @RequestMapping("/testUp")
+    public String testUp(MultipartFile photo, HttpSession session) throws IOException {
+        //获取上传的文件的文件名
+        String fileName = photo.getOriginalFilename();
+        //处理文件重名问题
+        String hzName = fileName.substring(fileName.lastIndexOf("."));
+        fileName = UUID.randomUUID().toString() + hzName;
+        //获取服务器中photo目录的路径
+        ServletContext servletContext = session.getServletContext();
+        String photoPath = servletContext.getRealPath("photo");
+        System.out.println(photoPath);
+        File file = new File(photoPath);
+        if(!file.exists()){
+            file.mkdir();
+        }
+        String finalPath = photoPath + File.separator + fileName;
+        //实现上传功能
+        photo.transferTo(new File(finalPath));
+        return "success";
+    }
+
+
+}
